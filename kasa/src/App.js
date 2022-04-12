88a45c8c37a6c60e8ab53a23f5d564ace51da150
sebastien-d-fr <contact@sebastien-d.me>
2022-04-12 19:16:24 +0200

    Fix du router

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,24 +1,11 @@
+import React from "react";
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../src/layouts/Layout";
-import Accueil from "./pages/Accueil/Accueil";
-import Fiche from './pages/Fiche-logement/Fiche';
-import APropos from "./pages/A-propos/APropos";
-import Erreur404 from "./pages/404/404";
+import RoutesPath from "../src/routes/RoutesPath";
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <Layout>
-                    <Routes>
-                        <Route path="/" element={<Accueil />}/>
-                        <Route path="/logement/:id" element={<Fiche />}/>
-                        <Route path="/a-propos" element={<APropos />}/>
-                        <Route path="*" element={<Erreur404 />}/>
-                    </Routes>
-                </Layout>
-            </BrowserRouter>
+            <RoutesPath/>
         </div>
     );
 }
--- kasa/src/components/Tuile/Tuile.js
+++ kasa/src/components/Tuile/Tuile.js
@@ -4,7 +4,7 @@ import "./Tuile.css";
 function Tuile({id, image, titre}) {
     return(
         <div className="tuile" id={id}>
-            <img className="tuile-image" src={image}/>
+            <img className="tuile-image" src={image} alt="Tuile"/>
             <div className="tuile-sombre"></div>
             <span className="tuile-titre">{titre}</span>
         </div>
